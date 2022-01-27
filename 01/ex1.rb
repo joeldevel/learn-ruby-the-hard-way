c02e6410d819eaccfe331cebc80751a81f3742a1
joeldevel <christianjoel.developer@gmail.com>
2022-01-27 20:51:40 -0300

    ex1

---
+++ 01/ex1.rb
@@ -0,0 +1,9 @@
+puts "Hello Worls!"
+puts "Hello Again"
+puts "I like typing this."
+puts "This is fun."
+puts "Yay! Printing"
+puts "I'd much rather you 'not'"
+puts 'I "said" do not touch this.'
+puts "This line is my line. It's my own invention"
+puts "This is how you print an emoji in ruby \u{1F63A}"
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # learn-ruby-the-hard-way
 Following Zed Shaw's tutorial  at https://learnrubythehardway.org
+
+## Exercises
+
+- [x] Exercise 1: A Good First Program
